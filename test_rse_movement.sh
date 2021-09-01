947541c69cdfa6e70efaf5242294a12108daaca4
Brandon White <bjwhite@fnal.gov>
2021-09-01 17:13:51 +0000

    Update to be more parameterized and allow for future expansion.

--- test_rse_movement.sh
+++ test_rse_movement.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# This script automates test data movement between the test RSEs DCACHE_BJWHITE_START and DCACHE_BJWHITE_END
-data_dir=$1
+# This script tests that Rucio is taking file uploads, and transferring files appropriately
+data_dir=/tmp/rucio_status_test.$(uuidgen)
+dataset_name=rucio_transfer_test_$(uuidgen)
+end_rse=DCACHE_BJWHITE_END
+num_files=5
+rucio_user=root
+start_rse=DCACHE_BJWHITE_START
 
-echo "Uploading files from $data_dir"
-for f in $(ls $data_dir); do
-    echo $f
+START=0
+END=4
+file_size=1024
+
+echo "Using data dir: ${data_dir}"
+mkdir ${data_dir}
+ 
+echo "Generating files to be uploaded."
+for (( c=$START; c<=$END; c++ ))
+do
+    name=$(uuidgen)
+    dd if=/dev/zero of=$data_dir/$name bs=$file_size count=1
 done
-echo
 
 # Upload the test files into Rucio
 for f in $(ls $data_dir); do
     echo "Uploading $data_dir/$f"
-    if ! rucio upload --rse DCACHE_BJWHITE_START $data_dir/$f; then
+    if ! rucio upload --rse $start_rse $data_dir/$f; then
         exit 1
     fi
 done
@@ -20,33 +33,37 @@ echo "Uploaded test files to DCACHE_BJWHITE_START RSE."
 
 # Add a dataset for the files
 # First create DID file
+echo "${data_dir}"
 for f in $(ls $data_dir); do
-    echo "user.root:$f" >> /tmp/tmpdids
+    echo "${data_dir}"
+    echo "Adding DID to: ${data_dir}/tmdids"
+    echo "user.${rucio_user}:${f}" >> ${data_dir}/tmpdids
 done
 
-echo "Creating user.root:rucio_test_files dataset and adding files"
-rucio add-dataset user.root:rucio_test_files
-if ! rucio attach user.root:rucio_test_files -f /tmp/tmpdids; then
-    exit 1
-fi
-rm /tmp/tmpdids
-
-# Add rule to move the dataset from DCACHE_BJWHITE_START to DCACHE_BJWHITE_END 
-echo "Making a rule to start the transfer of user.root:rucio_test_files -> DCACHE_BJWHITE_END"
-if ! rucio add-rule user.root:rucio_test_files 1 DCACHE_BJWHITE_END; then
+echo "Creating dataset ${dataset_name} and adding files"
+rucio add-dataset user.${rucio_user}:${dataset_name}
+if ! rucio attach user.${rucio_user}:${dataset_name} -f ${data_dir}/tmpdids; then
     exit 1
 fi
 
-# Check transfers submitted
+# Add rule to move the dataset from $start_rse to $end_rse
+echo "Making a rule to start the transfer of user.${rucio_user}:${dataset_name} -> DCACHE_BJWHITE_END"
+if ! rucio add-rule user.${rucio_user}:${dataset_name} 1 ${end_rse}; then
+    exit 1
+fi
+
+# Subscribe to the STOMP broker and wait for notifiations that the transfers have been completed
 # TODO
-echo "Sleeping for 120 seconds to let the transfers work."
-sleep 120
 
 # Check file locations include both DCACHE_BJWHITE_START and DCACHE_BJWHITE_END
-echo "Printing the file replicas for verification"
-for f in $(ls $data_dir); do
-    if ! rucio list-file-replicas user.root:$f; then
-        exit 1
-    fi
-done
+#echo "Printing the file replicas for verification"
+#for f in $(ls $data_dir); do
+#    if ! rucio list-file-replicas user.root:$f; then
+#        exit 1
+#    fi
+#done
 
+all_done=0
+if [ $all_done == 0 ]; then
+    rm -r $data_dir
+fi
