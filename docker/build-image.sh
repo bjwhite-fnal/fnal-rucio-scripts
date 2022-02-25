adddae10275d0698ca6ab68865af2ff42fdab710
Brandon White <bjwhite@fnal.gov>
2022-02-25 08:07:13 -0700

    Add ability to switch freely between Podman and Docker with ocirunner argument

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
+export ocirunner=docker
 
 ver="1.26.9"
 
 echo "Building with version: ${ver}"
 
-podman build --no-cache \
+${ocirunner} build --no-cache \
     -t donkeyman \
     --build-arg rucio_version=${ver} \
     ${PWD}
--- docker/run_rucio_client.sh
+++ docker/run_rucio_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start a Rucio client container
-# ./run_rucio_client <experiment> <rucio_account> <user> <id> <fermihost>
+# ./run_rucio_client <experiment> <rucio_account> <user> <id>
 #
 # Brandon White (bjwhite@fnal.gov)
 
@@ -15,7 +15,7 @@ usage()
     echo -e "\trucio_account     Account to be used for Rucio. Default: root"
     echo -e "\tuser     FNAL Linux username used for ssh. Default: bjwhite"
     echo -e "\tid     FNAL user id number. Default: 51660"
-    echo -e "\tfermihost     Fermilab machine to use for remote access to proxy initialization and grid certificates. Default: fermicloud523.fnal.gov"
+    echo -e "\tocirunner     podman or docker. Default: docker"
     echo
     echo -e "\tOptional"
     echo -e "\t-h    Display help"
@@ -36,7 +36,7 @@ experiment=${1:-int}
 rucio_account=${2:-root}
 user=${3:-bjwhite}
 id=${4:-51660}
-fermihost=${5:-fermicloud523.fnal.gov}
+ocirunner=${5:-docker}
 cert=x509up_u${id}
 rucio_account=${rucio_account}
 
@@ -64,15 +64,14 @@ else
 	exit -1
 fi
 
-container=$(podman run \
+container=$(${ocirunner} run \
 	-e RUCIO_CFG_RUCIO_HOST=${server_host} \
 	-e RUCIO_CFG_AUTH_HOST=${auth_host} \
 	-e RUCIO_CFG_AUTH_TYPE=x509_proxy \
         -e RUCIO_CFG_CLIENT_X509_PROXY=/tmp/x509up_u1000 \
 	-e RUCIO_CFG_ACCOUNT=${rucio_account} \
         -e VOMS_STR=${voms_str} \
-        -v ${cert}:/opt/certs/hostcert.pem \
-        -v ${cert}:/opt/certs/hostkey.pem \
+        -v certs:/opt/certs:Z \
 	--name=rucio-client-${experiment} \
 	-it -d donkeyman)
-podman cp ${PWD}/certificates ${container}:/etc/grid-security/certificates
+${ocirunner} cp ${PWD}/certificates ${container}:/etc/grid-security/certificates
